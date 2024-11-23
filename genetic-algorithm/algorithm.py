ea581acff7ef53123173933fa58ba51855f71ed2
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-11-23 21:02:04 +0100

    genetic-algorithm: selection methods
    roulette, rank

--- genetic-algorithm/algorithm.py
+++ genetic-algorithm/algorithm.py
@@ -4,7 +4,7 @@ import random
 class Individual:
     def __init__(self, chromosome_binary_list):
         self.chromosome = chromosome_binary_list
-        self.weight = 0     # value of fitness function
+        self.weight = 0  # value of fitness function
         self.value = 0
 
 
@@ -16,13 +16,15 @@ class geneticAlgorithm:
         self.population_size = population_size
         self.population = self.__generate_starting_population()
 
-
     def run(self):
         # print(self.population[0].chromosome)
         # self.__fitness(self.population[0])
         for i in self.population:
             self.__fitness(i)
-        self.__roulette_selection()
+        # self.__roulette_selection()
+        self.__rank_selection()
+        # print(len(self.population))
+        # print(self.population_size)
 
     def __generate_starting_population(self):
         population = []
@@ -40,14 +42,52 @@ class geneticAlgorithm:
         if weight <= self.backpack_capacity:
             individual.weight = weight
             individual.value = value
-        print(individual.weight, individual.value)
+        # print(individual.weight, individual.value)
 
     def __roulette_selection(self):
-        print('roulette')
+        # print('roulette')
         fitness_sum = sum(individual.weight for individual in self.population)
-        probabilities = []
-        for i in self.population:
-            probabilities.append(i.weight / fitness_sum)
+        # if fitness_sum == 0: ...
+        probabilities = [i.weight / fitness_sum for i in self.population]
+        cumulative_probability = [sum(probabilities[:i + 1]) for i in range(len(probabilities))]
+        # print(probabilities)
+        # print(cumulative_probability)
+        selected_individuals = []
+        for _ in range(self.population_size):
+            r = random.random()
+            for i, individual in enumerate(self.population):
+                if r <= cumulative_probability[i]:
+                    selected_individuals.append(individual)
+                    break
 
-        print(probabilities)
-        print(fitness_sum)
+        self.population = selected_individuals
+
+    def __rank_selection(self):
+        # print('rank selection')
+        sorted_population = sorted(self.population, key=lambda individual: individual.weight, reverse=True)
+
+        rank_sum = sum(range(1, self.population_size + 1))  # sum of an arithmetic series
+        probabilities = [(self.population_size - rank) / rank_sum for rank in range(self.population_size)]
+        cumulative_probability = [sum(probabilities[:i + 1]) for i in range(len(probabilities))]
+
+        # Baker's linear ranking selection
+        # sp = 1.0  # selection pressure, typically between 1 and 2
+        # rank_sum = self.population_size * (self.population_size + 1) / 2  # sum of an arithmetic series
+        # probabilities = [
+        #     (2 - sp) / self.population_size + 2 * rank * (sp - 1) / (self.population_size * (self.population_size - 1))
+        #     for rank in range(self.population_size)]
+        # cumulative_probability = [sum(probabilities[:i + 1]) for i in range(len(probabilities))]
+
+        # print(rank_sum)
+        # print(probabilities)
+        # print(cumulative_probability)
+
+        selected_individuals = []
+        for _ in range(self.population_size):
+            r = random.random()
+            for i, individual in enumerate(sorted_population):
+                if r <= cumulative_probability[i]:
+                    selected_individuals.append(individual)
+                    break
+
+        self.population = selected_individuals
--- genetic-algorithm/main.py
+++ genetic-algorithm/main.py
@@ -2,9 +2,11 @@ from algorithm import geneticAlgorithm
 from data import items
 
 if __name__ == '__main__':
-    ga = geneticAlgorithm(items, 5)
+    ga = geneticAlgorithm(items, 4)
     ga.run()
+
 
 # TODO co zrobic gdy mamy wiele osobnikow z fitness = 0
 # * rodzic moze byc rodzicem wiecej niz raz
 # ? czy podczas selekcji powinien moc byc wybrany wiecej niz raz?
+# * dodać selekcje rankingową jako druga metoda
